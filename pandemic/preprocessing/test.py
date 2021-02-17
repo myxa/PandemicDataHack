48388588ec9e33fd7b32b0a410b9c75ca84c3842
myxa <33979138+myxa@users.noreply.github.com>
2021-02-17 19:00:45 +0300

    Delete test.py

--- pandemic/preprocessing/test.py
+++
@@ -1,4 +0,0 @@
-from preprocess import Preprocess
-from data_utils import download_from_google_disc
-pr = Preprocess()
-df = pr.opener('C:\\Users\\Acer\\PycharmProjects\\PandemicDataHack\\pandemic\\data\\train.csv')
