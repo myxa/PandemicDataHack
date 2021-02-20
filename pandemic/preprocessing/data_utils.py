b0c1293238455e0313b423f4431a580f4c1b67b3
Ekaterina Kochedykova <ekaterinaaleksandrova7058@gmail.com>
2021-02-20 22:30:50 +0500

    помогите

--- pandemic/preprocessing/data_utils.py
+++ pandemic/preprocessing/data_utils.py
@@ -4,7 +4,7 @@ from pathlib import Path
 
 
 def download_from_google_disc(file_id=None, filename='train', dest=None):
-    # dictionary for datasets name and ids
+    # dictionary for datasets name and id
     id_dict = {'train.csv': '1P_C55GbIBTV_ZW_bTjiX26yY4p5EQj3H',
                'worldskills.csv': '1JUiJYHf-2nOfgoAaV6KYDKZx7HpgnsyZ'}
 
