6bb59de37cfc658d574f2b2c39cd681460f20887
myxa <33979138+myxa@users.noreply.github.com>
2021-02-20 20:21:25 +0300

    добрый вечер

--- pandemic/preprocessing/data_utils.py
+++ pandemic/preprocessing/data_utils.py
@@ -3,18 +3,18 @@ from google_drive_downloader import GoogleDriveDownloader as gdd
 from pathlib import Path
 
 
-def download_from_google_disc(file='train', dest=None):
-    if file == 'train':
+def download_from_google_disc(file_id=None, filename='train'):
+    if filename == 'train' and file_id is None:
         file_id = '1P_C55GbIBTV_ZW_bTjiX26yY4p5EQj3H'
-        if dest is None:
-            dest = Path(os.getcwd()).parent / 'data' / 'train.csv'
-    elif file == 'worldskills':
+        dest = Path(os.getcwd()).parent / 'data' / 'train.csv'
+    elif filename == 'worldskills' and file_id is None:
         file_id = '1JUiJYHf-2nOfgoAaV6KYDKZx7HpgnsyZ'
-        if dest is None:
-            dest = Path(os.getcwd()).parent / 'data' / 'worldskills.csv'
+        dest = Path(os.getcwd()).parent / 'data' / 'worldskills.csv'
     else:
         raise NotImplementedError
 
     gdd.download_file_from_google_drive(file_id=file_id,
                                         dest_path=dest, showsize=True)
+
+download_from_google_disc()
 
